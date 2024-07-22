2736ee40e5b9c88930ccca0d562c0d50acda1ca2
Juan Sebastian Sanchez L. <js.sanchezl12@uniandes.edu.co>
2024-07-22 12:09:30 -0500

    feat: all main products, loader component

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import { Loader } from "app/components/shared/Loader/Loader";
+
+export default function Loading() {
+  return (
+    <div>
+      < Loader />
+    </div>
+  );
+}
--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,26 +1,37 @@
+import Image from 'next/image'
+import styles from './MainProducts.module.sass'
+
 const getProducts = async () => {
-  const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2024-07/products.json`,{
-    headers: new Headers({
-      'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY ?? ''
+  try {
+    const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`, {
+      headers: new Headers({
+        'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || ""
+      })
     })
-  });
-  const products = await response.json();
-  return products;
-};
+    const { products } = await response.json()
+    return products
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 export const MainProducts = async () => {
-  console.log(process.env.SHOPIFY_HOSTNAME);
-  console.log(process.env.SHOPIFY_API_KEY);
-  const products = await getProducts();
-  console.log(products);
+  const products = await getProducts()
+  
   return (
-    <div className="main-products">
-      <h1>Main Products</h1>
-      <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
-      </ul>
-    </div>
-  );
+    <section className={styles.MainProducts}>
+      <h3>✨ New products released!</h3>
+      <div className={styles.MainProducts__grid}>
+        {products?.map((product:any) => {
+          const imageSrc = product.images[0].src;
+          return (
+            <article key={product.id}>
+              <p>{product.title}</p>
+              <Image src={imageSrc} fill alt={product.title} loading="eager" />
+            </article>
+          )
+        })}
+      </div>
+    </section>
+  )
 }
